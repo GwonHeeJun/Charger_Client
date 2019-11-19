10e8e7b49a6dd70d56bcaa23eb18301ec97eafa2
GwonHeeJun <gwonhj9751@naver.com>
2019-11-19 17:01:01 +0900

    [CREATE] Create components to create a membership page

---
+++ src/components/Auth/Register/Register.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import "./Register.scss";
+
+class Register extends Component {
+    render() {
+        return (
+            <div className="c-register">
+                <div className="c-register__header">
+                    <h1>회원가입</h1>
+                    <div className="c-register__header--process"></div>
+                </div>
+                <div className="c-register__content">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Register
