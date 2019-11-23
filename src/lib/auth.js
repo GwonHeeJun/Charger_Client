5c59ae548fbc7d2780f5f3fdc9e6094745b8a906
GwonHeeJun <gwonhj9751@naver.com>
2019-11-24 03:25:35 +0900

    [CREATE] Create Backend API in lib

---
+++ src/lib/auth.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const Default_URL =
+  "http://charger-env.i3arnwanuk.us-east-2.elasticbeanstalk.com/";
+
+export const Register = ({ id, password, name, email, phone }) =>
+  axios.post(Default_URL + "api/auth/register", {
+    id,
+    password,
+    name,
+    email,
+    phone
+  });
+
+  export const Login = ({ id, password }) =>
+  axios.post(Default_URL + "api/auth/login", {
+    id,
+    password
+  });
