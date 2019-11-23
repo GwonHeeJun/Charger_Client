721cc35901f2b2e6d7f119fa4d99f52f54bfe5d1
GwonHeeJun <gwonhj9751@naver.com>
2019-11-24 03:24:42 +0900

    [UPDATE] Update Signin Component in event & redux action

--- src/components/Auth/Signin/Signin.js
+++ src/components/Auth/Signin/Signin.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { isLogined, changeMenu } from "../../../stores/Layout/Layout.store";
+import { connect } from "react-redux";
+
+import * as Auth from "../../../lib/auth";
 import "./Signin.scss";
 
 class Signin extends Component {
@@ -6,40 +11,54 @@ class Signin extends Component {
     super(props);
 
     this.state = {
-      email: "",
+      id: "",
       password: ""
     };
+
+    this.onClickChangeMenu = this.onClickChangeMenu.bind();
+    this.onSubmitLogin = this.onSubmitLogin.bind();
   }
+
+  onSubmitLogin = (logined) => {
+    const { isLogined } = this.props;
+
+    isLogined(logined);
+  };
+
+  onClickChangeMenu = (menu) => {
+    const { changeMenu } = this.props;
+
+    changeMenu(menu);
+  };
 
   onChange = e => {
     const { name, value } = e.target;
     this.setState({
       [name]: value
     });
-    console.log(value);
   };
 
   onSubmit = e => {
     e.preventDefault();
-    // AuthApi.localLogin(
-    //     {
-    //         id: this.state.id,
-    //         password: this.state.password
-    //     }
-    // )
-    // .then((result) => {
-    //     console.log(result);
-    //     localStorage.setItem('thiscode-token', result.data.token);
-    //     this.props.userStore.Login();
-    //     this.setState({ redirect: true });
-    // })
-    // .catch((result) => {
-    //     console.log(result);
-    //     console.log('실패');
-    // });
+    Auth.Login({
+      id: this.state.id,
+      password: this.state.password
+    })
+      .then(result => {
+        console.log(result);
+        localStorage.setItem("charger-token", result.data.token);
+        this.onClickChangeMenu('main')
+        this.onSubmitLogin(true)
+      })
+      .catch(result => {
+        console.log("실패");
+      });
   };
 
   render() {
+    console.log(this.props);
+    
+
     return (
       <div className="c-signin">
         <div className="c-signin__wrapper">
@@ -48,10 +67,10 @@ class Signin extends Component {
           <div className="c-signin__wrapper--info"></div>
           <form onSubmit={this.onSubmit}>
             <input
-              name="email"
+              name="id"
               type="id"
-              placeholder="EMAIL"
-              value={this.state.email}
+              placeholder="ID"
+              value={this.state.id}
               onChange={this.onChange}
             />
             <input
@@ -64,14 +83,34 @@ class Signin extends Component {
             <button type="submit">로그인</button>
           </form>
           <div className="c-signin__wrapper--desc">
-            <span className="c-signin__wrapper--desc__findpw">비밀번호 찾기</span>
-            <span className="c-signin__wrapper--desc__regist">회원가입</span>
+            <span className="c-signin__wrapper--desc__findpw">
+              비밀번호 찾기
+            </span>
+            <span className="c-signin__wrapper--desc__regist"
+            onClick={() => this.onClickChangeMenu("register")}>
+              회원가입
+            </span>
           </div>
         </div>
-        <span className="c-signin__lock">혹시 해킹이 의심되는 상황인가요? 계정 잠금 ></span>
+        <span className="c-signin__lock">
+          혹시 해킹이 의심되는 상황인가요? 계정 잠금 >
+        </span>
       </div>
     );
   }
 }
 
-export default Signin;
+const mapStateToProps = state => {
+  return {
+    menu: state.layout.menu
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    changeMenu: menu => dispatch(changeMenu(menu)),
+    isLogined: logined => dispatch(isLogined(logined))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Signin);
