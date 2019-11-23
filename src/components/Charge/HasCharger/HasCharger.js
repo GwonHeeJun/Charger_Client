2d98bcbe3e72f5b2cceaaa8a1f0703be649dd82c
GwonHeeJun <gwonhj9751@naver.com>
2019-11-24 03:32:58 +0900

    [CREATE] Create Charge page List Component

---
+++ src/components/Charge/HasCharger/HasCharger.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./HasCharger.scss";
+
+class HasCharger extends Component {
+  render() {
+    return (
+      <div className="c-has-charger">
+        <div className="c-has-charger__wrapper">
+          <div className="c-has-charger__wrapper--money">
+            <span>총 보유자산</span>
+            <h2>0 원</h2>
+          </div>
+          <div className="c-has-charger__wrapper--type">
+              <span className="c-has-charger__wrapper--type__name">이름</span>
+              <span className="c-has-charger__wrapper--type__spacer"/>
+              <span className="c-has-charger__wrapper--type__price">현재 자산</span>
+          </div>
+          <div className="c-has-charger__wrapper--list">
+            <div className="c-has-charger__wrapper--list__krw">
+              <div className="row">
+                <div className="desc">
+                  <span>KRW</span>
+                </div>
+                <div className="type">
+                  <small>원화</small>
+                </div>
+              </div>
+              <div className="have">
+                  <span>0 원</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HasCharger;
