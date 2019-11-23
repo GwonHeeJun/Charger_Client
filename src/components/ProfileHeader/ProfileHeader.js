885a492b34ac4ead5a6136cc6010ec859485cf08
GwonHeeJun <gwonhj9751@naver.com>
2019-11-23 12:41:39 +0900

    [UPDATE] Update ProfileHeader redux action

--- src/components/ProfileHeader/ProfileHeader.js
+++ src/components/ProfileHeader/ProfileHeader.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { changeProfile } from '../../stores/Layout/Layout.store'
 import "./ProfileHeader.scss";
 
 class ProfileHeader extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+
+    }
+
+    this.onClickChangeProfile = this.onClickChangeProfile.bind();
+  }
+
+  onClickChangeProfile = (e, profile) => {
+    const { changeProfile } = this.props;
+    e.stopPropagation();
+
+    changeProfile(profile)
+  }
   render() {
+    const { profile } = this.props;
+    console.log(profile)
     return (
       <nav className="c-myheader">
         <div className="c-myheader__wrapper">
           <h1 className="c-myheader__wrapper--pagetitle">마이페이지</h1>
           <div className="c-myheader__wrapper--list">
-            <div className="c-myheader__wrapper--list__box-active">
-              <span className="c-myheader__wrapper--list__box-active--item">
+              <span className={profile === "activity" ? "c-myheader__wrapper--list__item-active" : "c-myheader__wrapper--list__item" }  onClick={e => this.onClickChangeProfile(e, "activity")}>
                 나의 활동
               </span>
-            </div>
-            <div className="c-myheader__wrapper--list__box">
-              <span className="c-myheader__wrapper--list__box--item">  
+              <span className={profile === "management" ? "c-myheader__wrapper--list__item-active" : "c-myheader__wrapper--list__item" }  onClick={e => this.onClickChangeProfile(e, "management")}>  
                 개인정보 관리
               </span>
-            </div>
           </div>
         </div>
       </nav>
@@ -25,4 +41,20 @@ class ProfileHeader extends Component {
   }
 }
 
-export default ProfileHeader;
+const mapStateToProps = (state) => {
+  return {
+    profile: state.layout.profile
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeProfile: (profile) => dispatch(changeProfile(profile))
+  }
+}
+
+export default connect( 
+  mapStateToProps,
+  mapDispatchToProps
+)(ProfileHeader)
+
