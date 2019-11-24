54eee3adde12caa4d07fab1bee9d05b64573a052
GwonHeeJun <gwonhj9751@naver.com>
2019-11-24 17:43:15 +0900

    [UPDATE] Update Signout style and connect redux

--- src/components/Charge/Charge.js
+++ src/components/Charge/Charge.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react'
-import ChargeLeft from './ChargeLeft/ChargeLeft'
-import './Charge.scss'
-import HasCharger from './HasCharger/HasCharger'
-import MainCharge from './MainCharge/MainCharge'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import ChargeLeft from "./ChargeLeft/ChargeLeft";
+import HasCharger from "./HasCharger/HasCharger";
+import MainCharge from "./MainCharge/MainCharge";
+import "./Charge.scss";
+import ChargeHistory from "./ChargerHistory/ChargeHistory";
 
 class Charge extends Component {
-    render() {
-        return (
-            <div className="c-charge">
-                <ChargeLeft />
-                <HasCharger />
-                <MainCharge />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="c-charge">
+        <ChargeLeft />
+        {this.props.chargeNav === "input" ? (
+          <>
+            <HasCharger />
+            <MainCharge />
+          </>
+        ) : (
+          <ChargeHistory />
+        )}
+      </div>
+    );
+  }
 }
 
-export default Charge
+const mapStateToProps = state => {
+  return {
+    chargeNav: state.layout.chargeNav
+  };
+};
+
+export default connect(mapStateToProps, null)(Charge);
