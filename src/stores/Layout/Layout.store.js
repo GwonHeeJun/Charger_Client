baa0bfa81c9b728caeada00d4d33e30926f93796
GwonHeeJun <gwonhj9751@naver.com>
2019-11-24 17:42:42 +0900

    [UPDATE] Update Redux layout action

--- src/stores/Layout/Layout.store.js
+++ src/stores/Layout/Layout.store.js
@@ -2,12 +2,17 @@ export const DEFAULT_ACTION = "DEFAULT_ACTION";
 export const CHANGE_MENU = "CHANGE_MENU";
 export const CHANGE_PROFILE = "CHANGE_PROFILE";
 export const IS_LOGINED = "IS_LOGINED";
+export const CHANGE_CHARGE_NAV = "CHANGE_CHARGE_NAV";
+export const CHOOSE_PAYMENT_TYPE = "CHOOSE_PAYMENT_TYPE";
+
 
 const initialState = {
   actions: "default",
   menu: "main",
   profile: "activity",
-  logined : false
+  logined : false,
+  chargeNav : "input",
+  payment : 1
 };
 
 /*********************************************
@@ -26,6 +31,14 @@ const initialState = {
  * 
  * logined : true       - 로그안 맞음
  *           false      - 로그인 아님
+ * 
+ * chargeNav : input    - 충전 
+ *             history  - 기록
+ * 
+ * payment : 1       - 카드
+ *           2      - 핸드폰
+ *           3    - 계좌 입금
+ *           4       - 테스트 결제
  *
  *********************************************/
 
@@ -50,6 +63,16 @@ export default function reducer(state = initialState, action = {}) {
       return {
         ...state,
         logined : action.payload.logined
+      }
+    case CHANGE_CHARGE_NAV:
+      return {
+        ...state,
+        chargeNav : action.payload.chargeNav
+      }
+    case CHOOSE_PAYMENT_TYPE:
+      return {
+        ...state,
+        payment : action.payload.payment
       }
     default:
       return state;
@@ -81,5 +104,19 @@ export const isLogined = logined => ({
   type: IS_LOGINED,
   payload: {
     logined
+  }
+})
+
+export const changeChargeNav = chargeNav => ({
+  type: CHANGE_CHARGE_NAV,
+  payload: {
+    chargeNav
+  }
+})
+
+export const choosePaymentType = payment => ({
+  type: CHOOSE_PAYMENT_TYPE,
+  payload: {
+    payment
   }
 })
