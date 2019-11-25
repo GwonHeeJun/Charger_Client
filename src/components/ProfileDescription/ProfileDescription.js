05cd444b90cab2447f61d76bfa5181e47d542ad7
GwonHeeJun <gwonhj9751@naver.com>
2019-11-25 13:29:09 +0900

    [UPDATE] Update User information

--- src/components/ProfileDescription/ProfileDescription.js
+++ src/components/ProfileDescription/ProfileDescription.js
@@ -1,7 +1,34 @@
 import React, { Component } from "react";
+import * as User from "../../lib/auth";
 import "./ProfileDescription.scss";
 
 class ProfileDescription extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      phone: "",
+      credit: 0,
+      elec: 0
+    };
+  }
+
+  componentDidMount() {
+    User.Check({
+      token: localStorage.getItem("charger-token")
+    }).then(res => {
+      console.log(res.data);
+      this.setState({
+        name : res.data.name,
+        email : res.data.email,
+        phone : res.data.phone.substr(0,3) + "-" + res.data.phone.substr(3,4) +"-"+ res.data.phone.substr(7,11),
+        credit : res.data.credit.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+        elec : res.data.electricity.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      });
+    });
+  }
   render() {
     return (
       <div className="c-profiledesc">
@@ -9,10 +36,10 @@ class ProfileDescription extends Component {
           <div className="c-profiledesc__wrapper--userbox">
             <div className="c-profiledesc__wrapper--userbox__greeting">
               <span className="c-profiledesc__wrapper--userbox__greeting--customer">
-                고객님
+                {this.state.name} 님
               </span>
               <span className="c-profiledesc__wrapper--userbox__greeting--withcharger">
-                Charger 서비스를 이용하신지 n일째입니다!
+                Charger 서비스를 이용해주셔서 감사합니다!
               </span>
             </div>
 
@@ -23,7 +50,7 @@ class ProfileDescription extends Component {
                     이메일
                   </span>
                   <span className="c-profiledesc__wrapper--userbox__infowrapper--myprofile__userinfo--useremail">
-                    skypedanny@naver.com
+                    {this.state.email}
                   </span>
                 </div>
 
@@ -32,7 +59,7 @@ class ProfileDescription extends Component {
                     이름
                   </span>
                   <span className="c-profiledesc__wrapper--userbox__infowrapper--myprofile__userinfo--username">
-                    이대훈
+                    {this.state.name}
                   </span>
                 </div>
 
@@ -41,18 +68,21 @@ class ProfileDescription extends Component {
                     휴대폰
                   </span>
                   <span className="c-profiledesc__wrapper--userbox__infowrapper--myprofile__userinfo--userphone">
-                    010-2613-2274
+                    {this.state.phone}
                   </span>
                 </div>
               </div>
 
               <div className="c-profiledesc__wrapper--userbox__infowrapper--remainbox">
-                <div className="c-mywallet__wrapper--userbox__infowrapper--remainbox__item" style={{marginBottom: "15px"}}>
+                <div
+                  className="c-mywallet__wrapper--userbox__infowrapper--remainbox__item"
+                  style={{ marginBottom: "15px" }}
+                >
                   <span className="c-profiledesc__wrapper--userbox__infowrapper--remainbox__item--desc">
                     현재 잔액
                   </span>
                   <span className="c-profiledesc__wrapper--userbox__infowrapper--remainbox__item--elec">
-                    10,000원
+                    {this.state.credit} 원
                   </span>
                 </div>
 
@@ -61,11 +91,10 @@ class ProfileDescription extends Component {
                     남은 전력
                   </span>
                   <span className="c-profiledesc__wrapper--userbox__infowrapper--remainbox__item--elec">
-                    100kW
+                    {this.state.elec} KW
                   </span>
                 </div>
               </div>
-            
             </div>
           </div>
         </div>
